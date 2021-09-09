56e18eb1a7c35d7edff25bc1eb6045708d8f5c9e
Shinrobin <shinrobin97@naver.com>
2021-09-09 17:04:57 +0900

    DRILL 03 완성
    2017180019 신재형

---
+++ 03/name.py
@@ -0,0 +1,107 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-100, 0)
+
+turtle.pendown()
+turtle.left(60)
+turtle.forward(50)
+turtle.right(120)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(-20, 0)
+
+turtle.pendown()
+turtle.left(150)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(-80, -30)
+
+turtle.pendown()
+turtle.left(180)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(70)
+
+turtle.penup()
+turtle.goto(20, 0)
+
+turtle.pendown()
+turtle.left(60)
+turtle.forward(50)
+turtle.left(120)
+turtle.forward(30)
+turtle.left(180)
+turtle.forward(60)
+turtle.left(180)
+turtle.forward(30)
+turtle.right(240)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(100,0)
+
+turtle.pendown()
+turtle.left(150)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(100, 25)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(15)
+
+turtle.penup()
+turtle.goto(115, 0)
+
+turtle.pendown()
+turtle.left(90)
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(160, 50)
+
+turtle.pendown()
+turtle.left(180)
+turtle.forward(15)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(180)
+turtle.forward(60)
+
+turtle.penup()
+turtle.goto(160, 20)
+
+turtle.pendown()
+turtle.circle(20)
+
+turtle.penup()
+turtle.goto(190, 15)
+
+turtle.pendown()
+turtle.left(180)
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(190, 0)
+
+turtle.pendown()
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(210,40)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(70)
+
+turtle.penup()
+turtle.goto(175, -50)
+
+turtle.pendown()
+turtle.circle(15)
+
+turtle.exitonclick()
