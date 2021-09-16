4d16af19b87c5e1e7f0795425cd364366413a0d1
Shinrobin <shinrobin97@naver.com>
2021-09-16 17:54:19 +0900

    DRILL #06 완성
    2017180019 신재형

--- res/character_moves.py
+++ res/character_moves.py
@@ -1,10 +1,20 @@
 from pico2d import *
+import math
+
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
+j = 270
+a = 400 + 200 * math.cos(math.radians(90))
+b = 90 + 200 * math.sin(math.radians(90))
 
-# fill here
+while(1):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400 + 200 * math.cos(math.radians(j)), 290 + 200 * math.sin(math.radians(j)))
+    j = j + 2
     
-close_canvas()
+    delay(0.01)
+
