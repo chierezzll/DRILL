3c4fa7aabdcc31c9eebd2867445402bbebaa64b2
Shinrobin <shinrobin97@naver.com>
2021-09-16 17:47:11 +0900

    DRILL #06 완성
    2017180019 신재형

--- 06/character_grass.py
+++ 06/character_grass.py
@@ -8,6 +8,7 @@ character = load_image('character.png')
 
 x = 400
 y = 90
+j = 270
 while(1):
     while x < 800:
         clear_canvas_now()
@@ -44,6 +45,12 @@ while(1):
         x += 4
         delay(0.01)
 
+    for j in range(0, 360):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(400 + 200 * math.cos(math.radians(j - 90)), 290 + 200 * math.sin(math.radians(j - 90)))
+        j = j + 2
+        delay(0.01)
 
 
 close_canvas()
